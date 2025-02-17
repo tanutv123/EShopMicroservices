66d69329dfc0efd75e313351b4411e6ec56e6318
tanutv123 <taitanvo2309@gmail.com>
2025-02-17 18:09:19 +0700

    Add docker compose file for Catalog's Postgres backing service

--- src/BuildingBlocks/BuildingBlocks/CQRS/ICommand.cs
+++ src/BuildingBlocks/BuildingBlocks/CQRS/ICommand.cs
@@ -4,7 +4,7 @@ namespace BuildingBlocks.CQRS
 {
     public interface ICommand : ICommand<Unit>
     {
-
+        
     }
     public interface ICommand<out TResponse> : IRequest<TResponse>
     {
--- src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
+++ src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
@@ -1,7 +1,4 @@
-﻿using BuildingBlocks.CQRS;
-using Catalog.API.Models;
-
-namespace Catalog.API.Products.CreateProduct
+﻿namespace Catalog.API.Products.CreateProduct
 {
     public record CreateProductCommand(
         string Name,
@@ -11,7 +8,7 @@ namespace Catalog.API.Products.CreateProduct
         decimal Price
         ) : ICommand<CreateProductResult>;
     public record CreateProductResult(Guid Id);
-    internal class CreateProductCommandHandler 
+    internal class CreateProductCommandHandler(IDocumentSession session)
         : ICommandHandler<CreateProductCommand, CreateProductResult>
     {
         public async Task<CreateProductResult> Handle(CreateProductCommand command, CancellationToken cancellationToken)
@@ -24,7 +21,10 @@ namespace Catalog.API.Products.CreateProduct
                 Image = command.Image,
                 Price = command.Price
             };
-            return new CreateProductResult(Guid.NewGuid());
+            session.Store(product);
+            await session.SaveChangesAsync(cancellationToken);
+
+            return new CreateProductResult(product.Id);
         }
     }
 }
