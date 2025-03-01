ee4c20abf9c141d2a84ef85640604eda415dc237
taitanvo2309@gmail.com <taitanvo2309@gmail.com>
2025-03-01 09:37:18 +0700

    Finish Ordering.Application's query operation and domain event handler

--- src/Services/Ordering/Ordering.Application/GlobalUsing.cs
+++ src/Services/Ordering/Ordering.Application/GlobalUsing.cs
@@ -4,3 +4,9 @@ global using Ordering.Application.Exceptions;
 global using BuildingBlocks.CQRS;
 global using Ordering.Application.Data;
 global using Ordering.Application.Dtos;
+global using MediatR;
+global using Microsoft.Extensions.Logging;
+global using Ordering.Domain.Events;
+global using Microsoft.EntityFrameworkCore;
+global using Ordering.Application.Extensions;
+
